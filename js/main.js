754518472d3446f3b3ce42ea12c63856d4b5c84e
virak ran <virak.ran@pnc.lan>
2019-12-10 09:23:54 +0700

    4 commit

--- js/main.js
+++ js/main.js
@@ -5,41 +5,66 @@ function getURL() {
 }
 $(document).ready(function () {
     requestdata();
+    $("#selection").on('change', function () {
+        var chooseRecipse = $("#selection").val();
+        recipesId(chooseRecipse);
+    })
 });
 
 function requestdata() {
     $.ajax({
         dataType: 'json',
         url: getURL(),
-        success: (data) => getRecipse(data),
+        success: (data) => getRecipse(data.recipes),
         error: () => console.error("Cannot request data"),
         error: () => console.log("Cannot get data")
     })
 }
+var allData = [];
 function getRecipse(data) {
     var option = "";
-    selectionRecipse(data);
-    data.recipes.forEach(element => {
+    allData = data;
+    data.forEach(element => {
         option += `<option value="${element.id}">${element.name}</option>`;
     });
     $("#selection").append(option);
 }
-function selectionRecipse(datas) {
-    datas.recipes.forEach(element => {
-        $("#selection").on('change', function() {
-            var chooseRecipse = $("#selection").val();
-            if(element.id == chooseRecipse) {
-                chooseRecipses(element);
-                element.ingredients.forEach(element => {
-                    getIngrediants(element);
-                });
-            }
-        })
-    });
+function recipesId(chooseRecipse) {
+    allData.forEach(item => {
+        if (item.id == chooseRecipse) {
+            chooseRecipses(item);
+            getIngrediants(item);
+        }
+    })
 }
 function chooseRecipses(item) {
-    console.log(item.name);
+    const { name, quantity, unit, iconUrl } = item;
+    var result = "";
+    result += `
+        <div class="col-2"></div>
+        <div class="col-4">
+            <h4 class="text-center mt-5">${name}</h4><br><br><br><br><br>
+            <h5 class="text-center">ingredients</h5>
+        </div>
+        <div class="col-4">
+        <img src="${iconUrl}" style="width:250px; height:170px">
+        </div>
+        <div class="col-2"></div>
+    `;
+    $("#result").html(result);
 }
-function getIngrediants(element){
-    console.log(element.name);
+function getIngrediants(item) {
+    var result = "";
+    item.ingredients.forEach(element => {
+        const { name, quantity, unit, iconUrl } = element;
+        result += `
+            <tr>
+            <td><img src="${iconUrl}" style="width:50px"></td>
+            <td>${quantity}</td>
+            <td>${unit}</td>
+            <td>${name}</td>
+            </tr>
+        `;
+    });
+    $("#ingredients").html(result);
 }
