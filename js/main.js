a2ac3b14e4d9ee373ea8aa879060d4b8fd16bb12
virak0001 <virakcambodia44@gmail.com>
2019-12-11 00:42:11 +0700

    5 commit

--- js/main.js
+++ js/main.js
@@ -34,17 +34,19 @@ function recipesId(chooseRecipse) {
         if (item.id == chooseRecipse) {
             chooseRecipses(item);
             getIngrediants(item);
+            numberOfPerson(item);
+            getInstruction(item);
         }
     })
 }
+
 function chooseRecipses(item) {
-    const { name, quantity, unit, iconUrl } = item;
+    const { name, iconUrl } = item;
     var result = "";
     result += `
         <div class="col-2"></div>
         <div class="col-4">
             <h4 class="text-center mt-5">${name}</h4><br><br><br><br><br>
-            <h5 class="text-center">ingredients</h5>
         </div>
         <div class="col-4">
         <img src="${iconUrl}" style="width:250px; height:170px">
@@ -53,6 +55,40 @@ function chooseRecipses(item) {
     `;
     $("#result").html(result);
 }
+
+function numberOfPerson(Guests) {
+    const{nbGuests} = Guests;
+    var person = "";
+    person += `
+    <div class="col-2 mb-3"></div>
+    <div class="col-4">
+        <h4>Number of Person</h4>
+    </div>
+    <div class="col-4">
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <button id="minus" type="button"
+                    class="btn btn-primary">&nbsp&nbsp&nbsp&nbsp-&nbsp&nbsp&nbsp&nbsp</button>
+            </div>
+            <input type="text" id="input" style="width:115px" class="text-center" value="${nbGuests}" disabled>
+            <div class="input-group-prepend">
+                <button type="button" id="sum"
+                    class="btn btn-success">&nbsp&nbsp&nbsp&nbsp+&nbsp&nbsp&nbsp&nbsp</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-2"></div><br><br><br>
+`;
+$("#people").html(person);
+$("#sum").on('click', function () {
+    var number = parseInt($("#input").val());
+    sum(number);
+})
+$("#minus").on('click', function () {
+    var number = parseInt($("#input").val());
+    minus(number);
+})
+}
 function getIngrediants(item) {
     var result = "";
     item.ingredients.forEach(element => {
@@ -60,11 +96,26 @@ function getIngrediants(item) {
         result += `
             <tr>
             <td><img src="${iconUrl}" style="width:50px"></td>
-            <td>${quantity}</td>
-            <td>${unit}</td>
+            <td id='quantity'>${quantity}</td>
+            <td>${unit[0]}</td>
             <td>${name}</td>
             </tr>
         `;
     });
     $("#ingredients").html(result);
+}
+function  getInstruction(item){
+
+}
+function sum(number) {
+    var add = parseInt(number) +1;
+    if(add <= 15) {
+        $("#input").val(add);
+    }
+}
+function minus(number) {
+    var minus = parseInt(number)-1;
+    if(minus >= 1) {
+        $("#input").val(minus);
+    }
 }
