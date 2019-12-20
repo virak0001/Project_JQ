a9121ebcdd7573bf72030c08d60f5570596cd02a
virak0001 <virakcambodia44@gmail.com>
2019-12-21 00:12:04 +0700

    change interface

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function getURL() {
     return url;
 }
 
+
 // jQuery
 $(document).ready(function () {
     // call function for request API
@@ -14,6 +15,7 @@ $(document).ready(function () {
         recipesId(chooseRecipse);
     })
 });
+
 
 // function for query API
 function requestdata() {
@@ -26,8 +28,10 @@ function requestdata() {
     })
 }
 
+
 //variable for store data in arrays that get from API
 var allData = [];
+
 // function for display the name of food on the selection option
 function getRecipse(data) {
     var option = "";
@@ -37,25 +41,35 @@ function getRecipse(data) {
     });
     $("#selection").append(option);
 }
+
+
 // variable for get old Guest
 var getQuanlity = [];
+
 // function for loop data from array variable
 var oldGuest = 0;
 function recipesId(chooseRecipse) {
     allData.forEach(item => {
         if (item.id == chooseRecipse) {
+
             // choose recipses 
             chooseRecipses(item);
+
             // get ingredients in recipse name
             getIngrediants(item);
+
             // get number of person 
             numberOfPerson(item);
+
             // get instruction
             getInstruction(item);
+
             // get quantity
             getQuanlities = item;
+
             //get OldGuest
             oldGuest = item.nbGuests;
+
         }
     })
 }
@@ -65,85 +79,69 @@ function chooseRecipses(item) {
     const { name, iconUrl } = item;
     var result = "";
     result += `
-        <div class="col-2"></div>
-        <div class="col-4">
-            <h4 class="text-center mt-5">${name}</h4><br><br><br><br><br>
-        </div>
-        <div class="col-4">
-        <img src="${iconUrl}" style="width:250px; height:170px">
-        </div>
-        <div class="col-2"></div>
+        <h5 class="text-center mt-5">${name}</h5><br>
+        <img src="${iconUrl}" style="width:280px; height:250px; border-radius:50%">
     `;
-    $("#result").html(result);
+    $("#img-recipes").html(result);
 }
 
- // get number of person 
+// get number of person 
 function numberOfPerson(Guests) {
-    const{nbGuests} = Guests;
+    const { nbGuests } = Guests;
     var person = "";
     person += `
-    <div class="col-2 mb-3"></div>
-    <div class="col-4">
-        <h4>Number of Person</h4>
-    </div>
-    <div class="col-4">
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <button id="minus" type="button"
-                    class="btn btn-primary">&nbsp&nbsp&nbsp&nbsp-&nbsp&nbsp&nbsp&nbsp</button>
-            </div>
-            <input type="text" id="input" style="width:115px" class="text-center" value="${nbGuests}" disabled>
-            <div class="input-group-prepend">
-                <button type="button" id="sum"
-                    class="btn btn-success">&nbsp&nbsp&nbsp&nbsp+&nbsp&nbsp&nbsp&nbsp</button>
-            </div>
-        </div>
-    </div>
-    <div class="col-2"></div><br><br><br>
+        <h5>Number of Person</h5>
+        <button id="minus" class="waves-effect waves-light btn" type="button">-</button>
+        <input  id="input" style="width:115px" class="center" value="${nbGuests}" disabled type="text">
+        <button type="button" id="sum" class="waves-effect waves-light btn">+</button>
 `;
-// diplay number of person in input value
-$("#people").html(person);
+    // diplay number of person in input value
+    $("#number_people").html(person);
 
-// function click on icon sum
-$("#sum").on('click', function () {
-    var number = parseInt($("#input").val());
-    sum(number);
-})
-// function click on icon minus
-$("#minus").on('click', function () {
-    var number = parseInt($("#input").val());
-    minus(number);
-})
+    // function click on icon sum
+    $("#sum").on('click', function () {
+        var number = parseInt($("#input").val());
+        sum(number);
+    })
+    // function click on icon minus
+    $("#minus").on('click', function () {
+        var number = parseInt($("#input").val());
+        minus(number);
+    })
 }
 
- // get ingredients in recipse name
- function getIngrediants(item) {
+// get ingredients in recipse name
+function getIngrediants(item) {
     var result = "";
     item.ingredients.forEach(element => {
         const { name, quantity, unit, iconUrl } = element;
         result += `
-            <tr>
-            <td><img src="${iconUrl}" style="width:50px"></td>
-            <td id='quantity'>${quantity}</td>
-            <td>${unit[0]}</td>
-            <td>${name}</td>
-            </tr>
+           <table class="striped">
+                <tr>
+                <td><img src="${iconUrl}" style="width:50px"></td>
+                <td id='quantity'>${quantity}</td>
+                <td>${unit[0]}</td>
+                <td>${name}</td>
+                </tr>
+           </table>
         `;
     });
     $("#ingredients").html(result);
 }
 // increase value when click on icon sum
 function sum(number) {
-    var add = parseInt(number) +1;
-    if(add <= 15) {
+    var add = parseInt(number) + 1;
+    if (add <= 15) {
         $("#input").val(add);
         getPerson($("#input").val());
     }
 }
+
+
 // decrease value when click on icon minus
 function minus(number) {
-    var minus = parseInt(number)-1;
-    if(minus >= 1) {
+    var minus = parseInt(number) - 1;
+    if (minus >= 1) {
         $("#input").val(minus);
         getPerson($("#input").val());
     }
@@ -152,12 +150,12 @@ function minus(number) {
 
 // get instruction
 function getInstruction(item) {
-    const{instructions} = item;
+    const { instructions } = item;
     var instruction = "";
     var step = instructions.split("<step>");
     for (let i = 1; i < step.length; i++) {
         instruction += `
-      <h5 class="text-primary">step ${i}:</h5>
+      <h5 class="blue-text">step ${i}:</h5>
              ${step[i]}
         `;
     }
@@ -172,17 +170,21 @@ function getPerson(person) {
     var newQuanlity;
     var result = "";
     getQuanlities.ingredients.forEach(element => {
-        var {quantity,iconUrl,name,unit} = element;
-        quantities = quantity/oldGuest;
-        newQuanlity = quantities*person;
+        var { quantity, iconUrl, name, unit } = element;
+        quantities = quantity / oldGuest;
+        newQuanlity = quantities * person;
         result += `
-        <tr>
-        <td><img src="${iconUrl}" style="width:50px"></td>
-        <td id='quantity'>${newQuanlity}</td>
-        <td>${unit[0]}</td>
-        <td>${name}</td>
-        </tr>
+        <table class="striped">
+            <tr>
+            <td><img src="${iconUrl}" style="width:50px"></td>
+            <td id='quantity'>${newQuanlity}</td>
+            <td>${unit[0]}</td>
+            <td>${name}</td>
+            </tr>
+        </table>
     `;
     });
-     $("#ingredients").html(result);
+    $("#ingredients").html(result);
 }
+
+
